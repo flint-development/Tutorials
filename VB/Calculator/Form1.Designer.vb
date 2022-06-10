66c0999cd8b02dd042887d5ee6d7bb932890fa6f
Flint01 <86834896+Flint01@users.noreply.github.com>
2022-06-10 20:25:25 +0200

    Add files via upload

---
+++ VB/Calculator/Form1.Designer.vb
@@ -0,0 +1,350 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.SIEBEN = New System.Windows.Forms.Button()
+        Me.ACHT = New System.Windows.Forms.Button()
+        Me.NEUN = New System.Windows.Forms.Button()
+        Me.SECHS = New System.Windows.Forms.Button()
+        Me.FÜNF = New System.Windows.Forms.Button()
+        Me.VIER = New System.Windows.Forms.Button()
+        Me.DREI = New System.Windows.Forms.Button()
+        Me.ZWEI = New System.Windows.Forms.Button()
+        Me.EINS = New System.Windows.Forms.Button()
+        Me.DEL = New System.Windows.Forms.Button()
+        Me.AC = New System.Windows.Forms.Button()
+        Me.PLUS = New System.Windows.Forms.Button()
+        Me.MINUS = New System.Windows.Forms.Button()
+        Me.KOMMA = New System.Windows.Forms.Button()
+        Me.NULL = New System.Windows.Forms.Button()
+        Me.GETEILT = New System.Windows.Forms.Button()
+        Me.MAL = New System.Windows.Forms.Button()
+        Me.GLEICH = New System.Windows.Forms.Button()
+        Me.TB = New System.Windows.Forms.TextBox()
+        Me.ROUND = New System.Windows.Forms.Button()
+        Me.EINSdurchX = New System.Windows.Forms.Button()
+        Me.PI = New System.Windows.Forms.Button()
+        Me.E = New System.Windows.Forms.Button()
+        Me.lbl2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'SIEBEN
+        '
+        Me.SIEBEN.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.SIEBEN.Location = New System.Drawing.Point(12, 74)
+        Me.SIEBEN.Name = "SIEBEN"
+        Me.SIEBEN.Size = New System.Drawing.Size(40, 40)
+        Me.SIEBEN.TabIndex = 0
+        Me.SIEBEN.Text = "7"
+        Me.SIEBEN.UseVisualStyleBackColor = False
+        '
+        'ACHT
+        '
+        Me.ACHT.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.ACHT.Location = New System.Drawing.Point(58, 74)
+        Me.ACHT.Name = "ACHT"
+        Me.ACHT.Size = New System.Drawing.Size(40, 40)
+        Me.ACHT.TabIndex = 1
+        Me.ACHT.Text = "8"
+        Me.ACHT.UseVisualStyleBackColor = False
+        '
+        'NEUN
+        '
+        Me.NEUN.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.NEUN.Location = New System.Drawing.Point(104, 74)
+        Me.NEUN.Name = "NEUN"
+        Me.NEUN.Size = New System.Drawing.Size(40, 40)
+        Me.NEUN.TabIndex = 2
+        Me.NEUN.Text = "9"
+        Me.NEUN.UseVisualStyleBackColor = False
+        '
+        'SECHS
+        '
+        Me.SECHS.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.SECHS.Location = New System.Drawing.Point(104, 120)
+        Me.SECHS.Name = "SECHS"
+        Me.SECHS.Size = New System.Drawing.Size(40, 40)
+        Me.SECHS.TabIndex = 5
+        Me.SECHS.Text = "6"
+        Me.SECHS.UseVisualStyleBackColor = False
+        '
+        'FÜNF
+        '
+        Me.FÜNF.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.FÜNF.Location = New System.Drawing.Point(58, 120)
+        Me.FÜNF.Name = "FÜNF"
+        Me.FÜNF.Size = New System.Drawing.Size(40, 40)
+        Me.FÜNF.TabIndex = 4
+        Me.FÜNF.Text = "5"
+        Me.FÜNF.UseVisualStyleBackColor = False
+        '
+        'VIER
+        '
+        Me.VIER.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.VIER.Location = New System.Drawing.Point(12, 120)
+        Me.VIER.Name = "VIER"
+        Me.VIER.Size = New System.Drawing.Size(40, 40)
+        Me.VIER.TabIndex = 3
+        Me.VIER.Text = "4"
+        Me.VIER.UseVisualStyleBackColor = False
+        '
+        'DREI
+        '
+        Me.DREI.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.DREI.Location = New System.Drawing.Point(104, 166)
+        Me.DREI.Name = "DREI"
+        Me.DREI.Size = New System.Drawing.Size(40, 40)
+        Me.DREI.TabIndex = 8
+        Me.DREI.Text = "3"
+        Me.DREI.UseVisualStyleBackColor = False
+        '
+        'ZWEI
+        '
+        Me.ZWEI.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.ZWEI.Location = New System.Drawing.Point(58, 166)
+        Me.ZWEI.Name = "ZWEI"
+        Me.ZWEI.Size = New System.Drawing.Size(40, 40)
+        Me.ZWEI.TabIndex = 7
+        Me.ZWEI.Text = "2"
+        Me.ZWEI.UseVisualStyleBackColor = False
+        '
+        'EINS
+        '
+        Me.EINS.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.EINS.Location = New System.Drawing.Point(12, 166)
+        Me.EINS.Name = "EINS"
+        Me.EINS.Size = New System.Drawing.Size(40, 40)
+        Me.EINS.TabIndex = 6
+        Me.EINS.Text = "1"
+        Me.EINS.UseVisualStyleBackColor = False
+        '
+        'DEL
+        '
+        Me.DEL.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.DEL.Location = New System.Drawing.Point(204, 74)
+        Me.DEL.Name = "DEL"
+        Me.DEL.Size = New System.Drawing.Size(40, 40)
+        Me.DEL.TabIndex = 9
+        Me.DEL.Text = "DEL"
+        Me.DEL.UseVisualStyleBackColor = False
+        '
+        'AC
+        '
+        Me.AC.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.AC.Location = New System.Drawing.Point(250, 74)
+        Me.AC.Name = "AC"
+        Me.AC.Size = New System.Drawing.Size(40, 40)
+        Me.AC.TabIndex = 10
+        Me.AC.Text = "AC"
+        Me.AC.UseVisualStyleBackColor = False
+        '
+        'PLUS
+        '
+        Me.PLUS.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.PLUS.Location = New System.Drawing.Point(204, 120)
+        Me.PLUS.Name = "PLUS"
+        Me.PLUS.Size = New System.Drawing.Size(40, 40)
+        Me.PLUS.TabIndex = 11
+        Me.PLUS.Text = "+"
+        Me.PLUS.UseVisualStyleBackColor = False
+        '
+        'MINUS
+        '
+        Me.MINUS.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.MINUS.Location = New System.Drawing.Point(250, 120)
+        Me.MINUS.Name = "MINUS"
+        Me.MINUS.Size = New System.Drawing.Size(40, 40)
+        Me.MINUS.TabIndex = 12
+        Me.MINUS.Text = "-"
+        Me.MINUS.UseVisualStyleBackColor = False
+        '
+        'KOMMA
+        '
+        Me.KOMMA.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.KOMMA.Location = New System.Drawing.Point(58, 212)
+        Me.KOMMA.Name = "KOMMA"
+        Me.KOMMA.Size = New System.Drawing.Size(40, 40)
+        Me.KOMMA.TabIndex = 14
+        Me.KOMMA.Text = ","
+        Me.KOMMA.UseVisualStyleBackColor = False
+        '
+        'NULL
+        '
+        Me.NULL.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.NULL.Location = New System.Drawing.Point(12, 212)
+        Me.NULL.Name = "NULL"
+        Me.NULL.Size = New System.Drawing.Size(40, 40)
+        Me.NULL.TabIndex = 13
+        Me.NULL.Text = "0"
+        Me.NULL.UseVisualStyleBackColor = False
+        '
+        'GETEILT
+        '
+        Me.GETEILT.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.GETEILT.Location = New System.Drawing.Point(250, 166)
+        Me.GETEILT.Name = "GETEILT"
+        Me.GETEILT.Size = New System.Drawing.Size(40, 40)
+        Me.GETEILT.TabIndex = 16
+        Me.GETEILT.Text = "/"
+        Me.GETEILT.UseVisualStyleBackColor = False
+        '
+        'MAL
+        '
+        Me.MAL.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.MAL.Location = New System.Drawing.Point(204, 166)
+        Me.MAL.Name = "MAL"
+        Me.MAL.Size = New System.Drawing.Size(40, 40)
+        Me.MAL.TabIndex = 15
+        Me.MAL.Text = "*"
+        Me.MAL.UseVisualStyleBackColor = False
+        '
+        'GLEICH
+        '
+        Me.GLEICH.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.GLEICH.Location = New System.Drawing.Point(204, 212)
+        Me.GLEICH.Name = "GLEICH"
+        Me.GLEICH.Size = New System.Drawing.Size(86, 40)
+        Me.GLEICH.TabIndex = 17
+        Me.GLEICH.Text = "="
+        Me.GLEICH.UseVisualStyleBackColor = False
+        '
+        'TB
+        '
+        Me.TB.Location = New System.Drawing.Point(12, 12)
+        Me.TB.Name = "TB"
+        Me.TB.Size = New System.Drawing.Size(278, 23)
+        Me.TB.TabIndex = 18
+        '
+        'ROUND
+        '
+        Me.ROUND.BackColor = System.Drawing.Color.Aqua
+        Me.ROUND.Location = New System.Drawing.Point(12, 268)
+        Me.ROUND.Name = "ROUND"
+        Me.ROUND.Size = New System.Drawing.Size(86, 40)
+        Me.ROUND.TabIndex = 21
+        Me.ROUND.Text = "Round"
+        Me.ROUND.UseVisualStyleBackColor = False
+        '
+        'EINSdurchX
+        '
+        Me.EINSdurchX.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.EINSdurchX.Location = New System.Drawing.Point(104, 268)
+        Me.EINSdurchX.Name = "EINSdurchX"
+        Me.EINSdurchX.Size = New System.Drawing.Size(40, 40)
+        Me.EINSdurchX.TabIndex = 20
+        Me.EINSdurchX.Text = "1/x"
+        Me.EINSdurchX.UseVisualStyleBackColor = False
+        '
+        'PI
+        '
+        Me.PI.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.PI.Location = New System.Drawing.Point(150, 268)
+        Me.PI.Name = "PI"
+        Me.PI.Size = New System.Drawing.Size(40, 40)
+        Me.PI.TabIndex = 19
+        Me.PI.Text = "PI"
+        Me.PI.UseVisualStyleBackColor = False
+        '
+        'E
+        '
+        Me.E.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.E.Location = New System.Drawing.Point(196, 268)
+        Me.E.Name = "E"
+        Me.E.Size = New System.Drawing.Size(40, 40)
+        Me.E.TabIndex = 22
+        Me.E.Text = "E"
+        Me.E.UseVisualStyleBackColor = False
+        '
+        'lbl2
+        '
+        Me.lbl2.AutoSize = True
+        Me.lbl2.Location = New System.Drawing.Point(12, 38)
+        Me.lbl2.Name = "lbl2"
+        Me.lbl2.Size = New System.Drawing.Size(41, 15)
+        Me.lbl2.TabIndex = 24
+        Me.lbl2.Text = "Label1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Silver
+        Me.ClientSize = New System.Drawing.Size(302, 336)
+        Me.Controls.Add(Me.lbl2)
+        Me.Controls.Add(Me.E)
+        Me.Controls.Add(Me.ROUND)
+        Me.Controls.Add(Me.EINSdurchX)
+        Me.Controls.Add(Me.PI)
+        Me.Controls.Add(Me.TB)
+        Me.Controls.Add(Me.GLEICH)
+        Me.Controls.Add(Me.GETEILT)
+        Me.Controls.Add(Me.MAL)
+        Me.Controls.Add(Me.KOMMA)
+        Me.Controls.Add(Me.NULL)
+        Me.Controls.Add(Me.MINUS)
+        Me.Controls.Add(Me.PLUS)
+        Me.Controls.Add(Me.AC)
+        Me.Controls.Add(Me.DEL)
+        Me.Controls.Add(Me.DREI)
+        Me.Controls.Add(Me.ZWEI)
+        Me.Controls.Add(Me.EINS)
+        Me.Controls.Add(Me.SECHS)
+        Me.Controls.Add(Me.FÜNF)
+        Me.Controls.Add(Me.VIER)
+        Me.Controls.Add(Me.NEUN)
+        Me.Controls.Add(Me.ACHT)
+        Me.Controls.Add(Me.SIEBEN)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "RECHNER"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents SIEBEN As Button
+    Friend WithEvents ACHT As Button
+    Friend WithEvents NEUN As Button
+    Friend WithEvents SECHS As Button
+    Friend WithEvents FÜNF As Button
+    Friend WithEvents VIER As Button
+    Friend WithEvents DREI As Button
+    Friend WithEvents ZWEI As Button
+    Friend WithEvents EINS As Button
+    Friend WithEvents DEL As Button
+    Friend WithEvents AC As Button
+    Friend WithEvents PLUS As Button
+    Friend WithEvents MINUS As Button
+    Friend WithEvents KOMMA As Button
+    Friend WithEvents NULL As Button
+    Friend WithEvents GETEILT As Button
+    Friend WithEvents MAL As Button
+    Friend WithEvents GLEICH As Button
+    Friend WithEvents TB As TextBox
+    Friend WithEvents ROUND As Button
+    Friend WithEvents EINSdurchX As Button
+    Friend WithEvents PI As Button
+    Friend WithEvents E As Button
+    Friend WithEvents lbl2 As Label
+End Class
---
+++ VB/Calculator/Form1.vb
@@ -0,0 +1,153 @@
+﻿Public Class Form1
+
+    Public Property zahl As String = ""
+    Public Property calc As String = ""
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles SIEBEN.Click
+        TB.Text += "7"
+
+    End Sub
+
+    Private Sub ACHT_Click(sender As Object, e As EventArgs) Handles ACHT.Click
+        TB.Text += "8"
+
+    End Sub
+
+    Private Sub Button15_Click(sender As Object, e As EventArgs) Handles NULL.Click
+        TB.Text += "0"
+
+    End Sub
+
+    Private Sub EINS_Click(sender As Object, e As EventArgs) Handles EINS.Click
+        TB.Text += "1"
+
+    End Sub
+
+    Private Sub ZWEI_Click(sender As Object, e As EventArgs) Handles ZWEI.Click
+        TB.Text += "2"
+
+    End Sub
+
+    Private Sub DREI_Click(sender As Object, e As EventArgs) Handles DREI.Click
+        TB.Text += "3"
+
+    End Sub
+
+    Private Sub VIER_Click(sender As Object, e As EventArgs) Handles VIER.Click
+        TB.Text += "4"
+
+    End Sub
+
+    Private Sub FÜNF_Click(sender As Object, e As EventArgs) Handles FÜNF.Click
+        TB.Text += "5"
+
+    End Sub
+
+    Private Sub SECHS_Click(sender As Object, e As EventArgs) Handles SECHS.Click
+        TB.Text += "6"
+
+    End Sub
+
+    Private Sub NEUN_Click(sender As Object, e As EventArgs) Handles NEUN.Click
+        TB.Text += "9"
+
+    End Sub
+
+    Private Sub DEL_Click(sender As Object, e As EventArgs) Handles DEL.Click
+        If (TB.Text.Length > 0) Then
+            TB.Text = TB.Text.Substring(0, TB.Text.Length - 1)
+        End If
+
+    End Sub
+
+    Private Sub AC_Click(sender As Object, e As EventArgs) Handles AC.Click
+        calc = ""
+        zahl = ""
+        TB.Text = ""
+
+        lbl2.Text = ""
+
+    End Sub
+
+    Private Sub GLEICH_Click(sender As Object, e As EventArgs) Handles GLEICH.Click
+        If (calc = "+") Then
+            TB.Text = Val(zahl) + Val(TB.Text)
+        End If
+        If (calc = "-") Then
+            TB.Text = Val(zahl) - Val(TB.Text)
+        End If
+        If (calc = "*") Then
+            TB.Text = Val(zahl) * Val(TB.Text)
+        End If
+        If (calc = "/") Then
+            TB.Text = Val(zahl) / Val(TB.Text)
+        End If
+
+
+        lbl2.Text = ""
+
+
+    End Sub
+
+    Private Sub MAL_Click(sender As Object, e As EventArgs) Handles MAL.Click
+        zahl = TB.Text
+        lbl2.Text = zahl
+        TB.Text = ""
+
+        calc = "*"
+
+    End Sub
+
+    Private Sub GETEILT_Click(sender As Object, e As EventArgs) Handles GETEILT.Click
+        zahl = TB.Text
+        lbl2.Text = zahl
+        TB.Text = ""
+
+        calc = "/"
+
+    End Sub
+
+    Private Sub PLUS_Click(sender As Object, e As EventArgs) Handles PLUS.Click
+        zahl = TB.Text
+        lbl2.Text = zahl
+        TB.Text = ""
+
+        calc = "+"
+
+    End Sub
+
+    Private Sub MINUS_Click(sender As Object, e As EventArgs) Handles MINUS.Click
+        zahl = TB.Text
+        lbl2.Text = zahl
+        TB.Text = ""
+
+        calc = "-"
+
+    End Sub
+
+    Private Sub KOMMA_Click(sender As Object, e As EventArgs) Handles KOMMA.Click
+        TB.Text += ","
+
+    End Sub
+
+    Private Sub ROUND_Click(sender As Object, e As EventArgs) Handles ROUND.Click
+        TB.Text = Math.Round(Val(TB.Text))
+
+    End Sub
+
+    Private Sub EINSdurchX_Click(sender As Object, e As EventArgs) Handles EINSdurchX.Click
+        TB.Text = 1.0 / Val(TB.Text)
+
+    End Sub
+
+    Private Sub PI_Click(sender As Object, e As EventArgs) Handles PI.Click
+        TB.Text = Math.PI
+
+    End Sub
+
+    Private Sub E_Click(sender As Object, e As EventArgs) Handles E.Click
+        TB.Text = Math.E
+
+    End Sub
+End Class
